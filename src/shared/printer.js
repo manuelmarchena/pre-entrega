a115733997ca8b9d2a46b53e3a5d9d31fb882d58
manuelmarchena <manuelmarche34@gmail.com>
2026-05-06 00:15:33 -0300

    "Se hacen mejoras sobre el printer"

--- src/shared/printer.js
+++ src/shared/printer.js
@@ -1,63 +1,53 @@
 
 export const outcomePrinter = ( action, result ) => {
     if ( action === "listProducts" ) {
-        return formater(titleAction, result)
+
+        return listCreater( result )
     }
     if ( action === "getProductById" ) {
-        const { id, title, price, description, category, image } = result
-        return console.log(`
-            Busqueda de items
-            Producto: ${id}
-            Título: ${title}
-            Precio: ${price}
-            descripcion: ${description}
-            categoria: ${category}
-            imagen URL: ${image}
-            `)
+        console.log("Búsqueda de productos")
+        return productBody(result)
     }
     if ( action === "createProduct" ) {
-        const { id, title, price, category, image} = result
-        return console.log(`
-            Creación de productos
-            Producto: ${id}
-            Título: ${title}
-            Precio: ${price}
-            descripcion: ${description}
-            categoria: ${category}
-            imagen URL: ${image}
-            `)
+       
+        console.log("Creación de productos")
+       return productBody(result)
     }
     if ( action === "deleteProductById" ) {
-        const { id, title, price, category, image} = result
-        return console.log(`
-            Creación de productos
-            Producto: ${id}
-            Título: ${title}
-            Precio: ${price}
-            descripcion: ${description}
-            categoria: ${category}
-            imagen URL: ${image}
-            `)
-        }
+
+        console.log("Borrado de productos")
+        return productBody(result)
+    }
+    return console.log(`No se pudo procesar el resultado de la acción. \n
+        ${result}`)
+
 }
 
-const formater = ( titleAction, result ) => {
-    
+const listCreater = ( result ) => {
+
+    console.log("Listado de productos")
     for ( const product of result ) {
-        console.log("Listado de productos")
         productBody(product)
     }
     
 }
 
-const productBody = (body) =>{
+const productBody = (body) => {
     const { id, title, price, description, category, image } = body
-    return console.log(`
-            Producto: ${id}
-            Título: ${title}
-            Precio: ${price}
-            descripcion: ${description}
-            categoria: ${category}
-            imagen URL: ${image}
-            `) 
-}
+
+    console.log(`Producto: ${id}`)
+    console.log(`Título: ${title}`)
+    console.log(`Precio: ${price}`)
+
+    if (description) {
+      console.log(`Descripción: ${description}`)
+    }
+
+    if (category) {
+      console.log(`Categoría: ${category}`)
+    }
+
+    if (image) {
+      console.log(`Imagen URL: ${image}`)
+    }
+  }
